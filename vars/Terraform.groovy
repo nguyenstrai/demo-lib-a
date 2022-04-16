12ae51166941edf00788ab482906bafcfff58b54
nguyenstrai <nguyenstrai@yahoo.com>
2022-04-16 14:14:32 -0400

    Update Terraform.groovy

--- vars/Terraform.groovy
+++ vars/Terraform.groovy
@@ -21,7 +21,7 @@ def call(body){
                 steps{
                     script{
                         dir("${env.WORKSPACE}/envs/develop"){
-                            withAWS(roleAccount:'992247318733', role:'arn:aws:iam::992247318733:role/demo-admin-role',credentials:'aws-user-jenkins') {
+                            withAWS(roleAccount:"${env.AWS_ACCOUNT_NUMBER}", role:"arn:aws:iam::${env.AWS_ACCOUNT_NUMBER}:role/demo-admin-role",credentials:'aws-user-jenkins') {
                             if (isUnix()){
                                 sh """terraform init  """
                             }
@@ -37,7 +37,7 @@ def call(body){
                 steps{
                     script{
                         dir("${env.WORKSPACE}/envs/develop"){
-                            withAWS(roleAccount:'992247318733', role:'arn:aws:iam::992247318733:role/demo-admin-role',credentials:'aws-user-jenkins') {
+                            withAWS(roleAccount:"${env.AWS_ACCOUNT_NUMBER}", role:"arn:aws:iam::${env.AWS_ACCOUNT_NUMBER}:role/demo-admin-role",credentials:'aws-user-jenkins') {
                             if (isUnix()){
                                 sh """terraform plan -var ec2_instance_type="${env.INSTANCE_TYPE}"  -var ec2_tag="${env.EC2_TAG}" -var key_name="${env.KEY_NAME}" -var environment="${env.ENVIRONMENT}" """
                             }
@@ -60,7 +60,7 @@ def call(body){
                 steps{
                     script{
                         dir("${env.WORKSPACE}/envs/develop"){
-                            withAWS(roleAccount:'992247318733', role:'arn:aws:iam::992247318733:role/demo-admin-role',credentials:'aws-user-jenkins') {
+                            withAWS(roleAccount:"${env.AWS_ACCOUNT_NUMBER}", role:"arn:aws:iam::${env.AWS_ACCOUNT_NUMBER}:role/demo-admin-role",credentials:'aws-user-jenkins') {
                             if (isUnix()){
                                 sh """terraform apply -var ec2_instance_type="${env.INSTANCE_TYPE}"  -var ec2_tag="${env.EC2_TAG}" -var key_name="${env.KEY_NAME}" -var environment="${env.ENVIRONMENT}" -auto-approve """
                             }
